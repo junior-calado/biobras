0786cd33fdffa549b3b1fefafc87adce6222fb12
ATAKONE\junior.calado <junior.calado@atak.com.br>
2025-06-10 14:28:14 -0300

    Verificando rota para o pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/biobras/',
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [react()],
+      base: '/',
+    }
+  } else {
+    return {
+      plugins: [react()],
+      base: '/biobras/',
+    }
+  }
 })
